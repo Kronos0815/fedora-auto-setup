8d2cb70c2ef780b94a02aa6943ce71700859c575
kronos <nils.schiele@uni-konstanz.de>
2025-05-20 07:57:48 +0200

    add a new script to install the theme

---
+++ scripts/install-theme.sh
@@ -0,0 +1,12 @@
+# Check if root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Please do not run this script as root. Run it as a normal user."
+    exit 1
+fi
+
+# install necessary packages
+
+sudo dnf install -y gtk-murrine-engine
+sudo dnf install -y sassc
+sudo dnf install -y gnome-themes-extra
+
